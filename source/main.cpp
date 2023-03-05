86e251113cc1db5c16c11a44b79452a8f32ce292
1poundSOCKS <mathew.coburn@gmail.com>
2023-03-05 08:46:00 +0000

    Improved error handling

--- source/main.cpp
+++ source/main.cpp
@@ -11,50 +11,109 @@ std::mutex g_heapMutex;
 
 void* operator new(size_t size)
 {
-  std::lock_guard<std::mutex> guard(g_heapMutex);
-  printf("new: malloc %i bytes\n", static_cast<int>(size));
-  auto buffer = g_memoryController.Allocate(size);
-  printf("new: heap map size=%i\n", static_cast<int>(g_memoryController.AllocationCount()));
-  return buffer;
+  try
+  {
+    std::lock_guard<std::mutex> guard(g_heapMutex);
+    printf("new: malloc %i bytes\n", static_cast<int>(size));
+    auto buffer = g_memoryController.Allocate(size);
+    printf("new: heap map size=%i\n", static_cast<int>(g_memoryController.AllocationCount()));
+    return buffer;
+  }
+  catch(const std::exception& e)
+  {
+    ::printf("error: %s\n", e.what());
+    return nullptr;
+  }
+  catch( heap_map::error_type )
+  {
+    ::printf("heap map error\n");
+    return nullptr;
+  }
 }
 
 void* operator new[](size_t size)
 {
-  std::lock_guard<std::mutex> guard(g_heapMutex);
-  printf("new[]: malloc %i bytes\n", static_cast<int>(size));
-  auto buffer = g_memoryController.Allocate(size);
-  printf("new: heap map size=%i\n", static_cast<int>(g_memoryController.AllocationCount()));
-  return buffer;
+  try
+  {
+    std::lock_guard<std::mutex> guard(g_heapMutex);
+    printf("new[]: malloc %i bytes\n", static_cast<int>(size));
+    auto buffer = g_memoryController.Allocate(size);
+    printf("new: heap map size=%i\n", static_cast<int>(g_memoryController.AllocationCount()));
+    return buffer;
+  }
+  catch(const std::exception& e)
+  {
+    ::printf("error: %s\n", e.what());
+    return nullptr;
+  }
+  catch( heap_map::error_type )
+  {
+    ::printf("heap map error\n");
+    return nullptr;
+  }
 }
 
 void operator delete(void* buffer)
 {
-  std::lock_guard<std::mutex> guard(g_heapMutex);
-  printf("delete\n");
-  g_memoryController.Free(buffer);
-  printf("delete: heap map size=%i\n", static_cast<int>(g_memoryController.AllocationCount()));
+  try
+  {
+    std::lock_guard<std::mutex> guard(g_heapMutex);
+    printf("delete\n");
+    g_memoryController.Free(buffer);
+    printf("delete: heap map size=%i\n", static_cast<int>(g_memoryController.AllocationCount()));
+  }
+  catch(const std::exception& e)
+  {
+    ::printf("error: %s\n", e.what());
+  }
+  catch( heap_map::error_type )
+  {
+    ::printf("heap map error\n");
+  }
 }
 
 void operator delete[](void* buffer)
 {
-  std::lock_guard<std::mutex> guard(g_heapMutex);
-  printf("delete[]\n");
-  g_memoryController.Free(buffer);
-  printf("delete: heap map size=%i\n", static_cast<int>(g_memoryController.AllocationCount()));
+  try
+  {
+    std::lock_guard<std::mutex> guard(g_heapMutex);
+    printf("delete[]\n");
+    g_memoryController.Free(buffer);
+    printf("delete: heap map size=%i\n", static_cast<int>(g_memoryController.AllocationCount()));
+  }
+  catch(const std::exception& e)
+  {
+    ::printf("error: %s\n", e.what());
+  }
+  catch( heap_map::error_type )
+  {
+    ::printf("heap map error\n");
+  }
 }
 
 int main(int argc, char* argv[])
 {
-  auto buffer = g_memoryController.Allocate(1024);
-
-  std::list<int> integerList;
-
-  for( int i = 0; i < 100; ++i )
+  try
   {
-    integerList.push_back(i);
-  }
+    auto buffer = g_memoryController.Allocate(1024);
 
-  g_memoryController.Free(buffer);
+    std::list<int> integerList;
+
+    for( int i = 0; i < 100; ++i )
+    {
+      integerList.push_back(i);
+    }
+
+    g_memoryController.Free(buffer);
+  }
+  catch( heap_map::error_type )
+  {
+    std::cerr << "heap map error\n";
+  }
+  catch(const std::exception& e)
+  {
+    std::cerr << e.what() << "\n";
+  }
 
   return 0;
 }
